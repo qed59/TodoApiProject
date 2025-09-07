5938425419241fd9aee7ff31afaa656b077cd13e
qed59 <qed59@hotmail.com>
2025-09-07 18:29:17 +1000

    Add project files.

---
+++ src/TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TodoController : ControllerBase
+    {
+        private static readonly List<TodoItem> _todos = new();
+        private static int _nextId = 1 ;
+
+        [HttpGet]
+        public ActionResult<IEnumerable<TodoItem>> GetAll()
+        {
+            return Ok(_todos);
+        }
+
+        [HttpPost]
+        public ActionResult<TodoItem> Create([FromForm] TodoItem item)
+        {
+            item.Id = _nextId++;
+            _todos.Add(item);
+            return CreatedAtAction(nameof(GetAll), new { id = item.Id }, item);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var todo = _todos.FirstOrDefault(t => t.Id == id);
+            if (todo == null) return NotFound();
+            _todos.Remove(todo);
+            return NoContent();
+        }
+    }
+}
---
+++ tests/APITestProject/TodoControllerTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoApi.Controllers;
+using TodoApi.Models;
+
+namespace APITestProject
+{
+    public class TodoControllerTests
+    {
+        [Fact]
+        public void Create_ShouldAddNewTodo()
+        {
+            // Arrange
+            var controller = new TodoController();
+            var newItem = new TodoItem { Title = "Test Task", IsComplete = false };
+
+            // Act
+            var result = controller.Create(newItem);
+
+            // Assert
+            var createdAt = Assert.IsType<CreatedAtActionResult>(result.Result);
+            var todo = Assert.IsType<TodoItem>(createdAt.Value);
+            Assert.Equal("Test Task", todo.Title);
+
+            // Check that GetAll returns the item
+            var getAll = controller.GetAll();
+            var okResult = Assert.IsType<OkObjectResult>(getAll.Result);
+            var todos = Assert.IsAssignableFrom<System.Collections.Generic.IEnumerable<TodoItem>>(okResult.Value);
+            Assert.Single(todos);
+        }
+
+        [Fact]
+        public void Delete_ShouldRemoveTodo()
+        {
+            // Arrange
+            var controller = new TodoController();
+            var newItem = new TodoItem { Title = "Delete Me", IsComplete = false };
+            var created = controller.Create(newItem);
+            var createdAt = Assert.IsType<CreatedAtActionResult>(created.Result);
+            var todo = Assert.IsType<TodoItem>(createdAt.Value);
+
+            // Act
+            var deleteResult = controller.Delete(todo.Id);
+
+            // Assert
+            Assert.IsType<NoContentResult>(deleteResult);
+
+            // Verify it's removed
+            var getAll = controller.GetAll();
+            var okResult = Assert.IsType<OkObjectResult>(getAll.Result);
+            var todos = Assert.IsAssignableFrom<System.Collections.Generic.IEnumerable<TodoItem>>(okResult.Value);
+            Assert.DoesNotContain(todos, t => t.Id == todo.Id);
+        }
+    }
+
+}
